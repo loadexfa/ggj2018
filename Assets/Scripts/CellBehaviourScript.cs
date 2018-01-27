82bff0f52a6be753be6751d7fc900dd931f27db5
Joe Miller <josephclaymiller@gmail.com>
2018-01-26 22:14:12 -0800

    Virus infects red blood

---
+++ Assets/Scripts/CellBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellBehaviourScript : MonoBehaviour {
+	public GameObject virus;
+	private Vector3 pos;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "virus") {
+			Debug.Log ("collided with virus");
+			pos = transform.position;
+			Destroy(gameObject);
+			Instantiate (virus);
+			virus.transform.position = pos;
+		}
+	}
+}
