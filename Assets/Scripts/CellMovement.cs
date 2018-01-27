998ff0280b3c0761ac6112ad53bd288a651db5a0
Josh Samuels <joshdsamuels@gmail.com>
2018-01-26 20:44:55 -0800

    Simple movement works now

---
+++ Assets/Scripts/CellMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellMovement : MonoBehaviour {
+
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+    void OnMouseDown() {
+        startPosition = Input.mousePosition;
+    }
+
+    private void OnMouseUp() {
+        endPosition = Input.mousePosition;
+        Vector3 displacement = endPosition - startPosition;
+        rigidBody.AddForce(new Vector2(displacement.x, displacement.y));
+    }
+}
