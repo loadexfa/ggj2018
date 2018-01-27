790af1e29d84a3f6226a935d3a55c92ed2bebc95
Joe Miller <josephclaymiller@gmail.com>
2018-01-27 12:25:44 -0800

    scene1 version2 art

--- Assets/Scripts/CellBehaviour.cs
+++ Assets/Scripts/CellBehaviour.cs
@@ -6,7 +6,6 @@ public class CellBehaviour : MonoBehaviour {
 	public Sprite redBloodCellSprite;
 	public Sprite whiteBloodCellSprite;
 	public Sprite virusSprite;
-	public bool isVirus;
 	private SpriteRenderer m_SpriteRenderer;
 	private Vector3 startPosition;
 	private Vector3 endPosition;
@@ -16,15 +15,14 @@ public class CellBehaviour : MonoBehaviour {
 	void Start()
 	{
 		m_rigidBody = this.GetComponent<Rigidbody2D> ();
-		if (isVirus) {
-			changeToVirus ();
-		}
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.tag == "virus") {
 			Debug.Log ("collided with virus");
-			changeToVirus ();
+			if (this.gameObject.tag == "red blood cell") {
+				changeToVirus ();
+			}
 		}
 	}
 
@@ -32,7 +30,6 @@ public class CellBehaviour : MonoBehaviour {
 		//Get the SpriteRenderer on this GameObject
 		m_SpriteRenderer = this.GetComponent<SpriteRenderer>();
 		m_SpriteRenderer.sprite = virusSprite;
-		isVirus = true;
 		this.tag = "virus";
 		this.GetComponent<CellMovement>().enabled = true;
 	}
