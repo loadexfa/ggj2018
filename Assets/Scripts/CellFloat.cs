fa53cf492d5959070269ec0c6a6eb682f94ed38d
Aryn Rozelle <aryn@raindropgames.com>
2018-01-27 16:48:30 -0800

    Cell float and horizontal movement

---
+++ Assets/Scripts/CellFloat.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellFloat : MonoBehaviour {
+
+    public enum direction { up, down };
+
+    public direction currentDirection;
+ 
+    Rigidbody2D m_rigidBody;
+
+    // Use this for initialization
+    void Start () {
+        m_rigidBody = this.GetComponent<Rigidbody2D>();
+        InvokeRepeating("ChangeDirection", 0, 1f);
+        
+    }	
+    void ChangeDirection()
+    {
+        if (currentDirection == direction.up)
+            currentDirection = direction.down;
+
+        else if (currentDirection == direction.down)
+            currentDirection = direction.up;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        if (currentDirection == direction.up)
+        { 
+            m_rigidBody.AddForce(Vector3.up * .5f);
+        }
+        else
+        {
+            m_rigidBody.AddForce(Vector3.down * .5f);
+        }
+           
+    }
+}
---
+++ Assets/Scripts/CellHorizontal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellHorizontal : MonoBehaviour {
+
+    public enum direction { right, left };
+
+    public direction currentDirection;
+
+    Rigidbody2D m_rigidBody;
+
+    // Use this for initialization
+    void Start () {
+        m_rigidBody = this.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (currentDirection == direction.right)
+        {
+            m_rigidBody.AddForce(Vector3.right * .1f);
+        }
+		
+	}
+}
