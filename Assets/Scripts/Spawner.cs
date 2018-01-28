cacb86a213a7d3a64eed29e91abac9179fc0e234
Josh Samuels <joshdsamuels@gmail.com>
2018-01-28 12:41:51 -0800

    We now detect the end of the game

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -18,16 +18,39 @@ public class Spawner : MonoBehaviour {
 	}
 	
     void SpawnWhiteCell() {
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-        GameObject cell = Instantiate(whiteCellPrefab, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-        cell.GetComponent<CellMovement>().teleportLeftDestination = leftBoundary;
-        cell.GetComponent<CellMovement>().teleportRightDestination = rightBoundary;
+        if (!DetectGameEnd()) {
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            GameObject cell = Instantiate(whiteCellPrefab, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            cell.GetComponent<CellMovement>().teleportLeftDestination = leftBoundary;
+            cell.GetComponent<CellMovement>().teleportRightDestination = rightBoundary;
+        }
     }
 
     void SpawnRedCell() {
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-        GameObject cell = Instantiate(redCellPrefab, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-        cell.GetComponent<CellMovement>().teleportLeftDestination = leftBoundary;
-        cell.GetComponent<CellMovement>().teleportRightDestination = rightBoundary;
+        if (!DetectGameEnd()) {
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            GameObject cell = Instantiate(redCellPrefab, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            cell.GetComponent<CellMovement>().teleportLeftDestination = leftBoundary;
+            cell.GetComponent<CellMovement>().teleportRightDestination = rightBoundary;
+        }
+    }
+
+    bool DetectGameEnd() {
+        GameObject red = GameObject.FindWithTag("red blood cell");
+        GameObject white = GameObject.FindWithTag("white blood cell");
+        GameObject virus = GameObject.FindWithTag("virus");
+
+        if (red == null && white == null) {
+            Debug.Log("you win!");
+            return true;
+        }
+
+        if (virus == null) {
+            Debug.Log("You lose!");
+            return true;
+        }
+
+        return false;
+
     }
 }
