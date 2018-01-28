213b9f00dca05d06362e290bb7257a1d62b97b5b
Josh Samuels <joshdsamuels@gmail.com>
2018-01-28 14:45:48 -0800

    Minor cleanup, integrated spawn points

--- Assets/Scripts/WhiteBloodAi.cs
+++ Assets/Scripts/WhiteBloodAi.cs
@@ -6,7 +6,7 @@ public class WhiteBloodAi : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other) {
         if (this.tag == "IgnoreBounds" && other.gameObject.tag == "virus") {
-            Debug.Log("White blood cell has detected virus");
+            //Debug.Log("White blood cell has detected virus");
         }
     }
 }
